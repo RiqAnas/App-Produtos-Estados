3d74bf22f16474105abffd8b9836ad0aeba392ed
Henrique Santos <henriqanastacioc@gmail.com>
2025-10-20 15:35:48 -0300

    Teste gitIgnore

--- lib/Utils/appRoutes.dart
+++
@@ -1,8 +0,0 @@
-class Approutes {
-  static const AUTHORHOMEPAGE = '/';
-  static const ORDERS = '/orders';
-  static const ITEMPRODUCT = '/itemproduct';
-  static const CART = '/cart';
-  static const EDITOR = '/editor';
-  static const PRODUCTFORM = '/productform';
-}
